21ea6e6808980dc07677de55c07d442ca4f4ca8d
CoderGirlBre <41300229+CoderGirlBre@users.noreply.github.com>
2018-07-19 21:01:10 -0500

    Add files via upload

---
+++ Calculating Area of a Rectangle/Rectangle Area.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's calculate the area of a rectangle!");
+            Console.Write("Enter a number to determine the length: ");
+            string length = Console.ReadLine();
+            Console.Write("Enter a number to determine the width: ");
+            string width = Console.ReadLine();
+
+            int lengthnumber = int.Parse(length);
+            int widthnumber = int.Parse(width);
+
+            int area = lengthnumber * widthnumber;
+            Console.WriteLine("The area of your rectangle is: " + area);
+            Console.ReadLine();
+
+        }
+    }
+}
