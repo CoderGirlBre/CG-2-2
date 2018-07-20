4ec7ece66b50024eb275231761e01512cd45fae3
Breona <breona.caldwell98@gmail.com>
2018-07-19 20:20:21 -0500

    Add project files.

---
+++ Calculating Area of a Rectangle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calculating_Area_of_a_Rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
